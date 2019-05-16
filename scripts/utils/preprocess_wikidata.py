24603a8176460e0392f201e2a78a31f057cd4ad6
Sophie Henning <s8sohenn@stud.uni-saarland.de>
2019-05-16 11:26:37 +0200

    Remove inconsistencies in usage of colon as a separator

--- scripts/utils/preprocess_wikidata.py
+++ scripts/utils/preprocess_wikidata.py
@@ -1,6 +1,15 @@
 import sys
 from time import time
 
+'''Compiles language-specific versions of a dictionary containing lines like
+en::Belgium|||de::Belgien|||es::Bélgica|||fr::Belgique
+The result will be four files each containing one of the lines:
+Belgium|||de:Belgien|||es:Bélgica|||fr:Belgique
+Belgien|||en:Belgium|||es:Bélgica|||fr:Belgique
+Belgique|||en::Belgium|||de::Belgien|||es::Bélgica
+Bélgica|||en::Belgium|||de::Belgien|||fr::Belgique
+'''
+
 
 def read_in(path_to_dict):
     dicts = dict()
@@ -17,7 +26,7 @@ def read_in(path_to_dict):
             for entry in entries:
                 split_ = entry.split("::")
                 la_code = split_[0]
-                term = split_[1]
+                term = split_[1].replace(":", " ").strip()
                 entries_with_languages[la_code] = term
 
             if first:
@@ -26,11 +35,16 @@ def read_in(path_to_dict):
                     dicts[la_code] = dict()
 
             for (la_code, term) in entries_with_languages.items():
-                dicts[la_code][term] = dict()
+                la_dict = dicts[la_code]
+
+                # do not overwrite existing entries
+                if term in la_dict:
+                    continue
+                la_dict[term] = dict()
                 for (trans_la_code, translation) in entries_with_languages.items():
                     if trans_la_code == la_code:
                         continue
-                    dicts[la_code][term][trans_la_code] = translation
+                    la_dict[term][trans_la_code] = translation
 
             num_entries += 1
             if num_entries % 100000 == 0:
@@ -43,15 +57,17 @@ def write_to_file(path_to_dict, dicts):
     num_entries = 0
     for (la_code, la_dict) in dicts.items():
         output_path = path_to_dict + "." + la_code
+        print("Writing dict for language code ", la_code)
         with open(output_path, 'w') as outp:
             for (term, translations) in la_dict.items():
                 outp.write(term)
                 for (trans_la_code, translation) in translations.items():
-                    outp.write("|||" + trans_la_code + "::" + translation)
+                    outp.write("|||" + trans_la_code + ":" + translation)
                 outp.write("\n")
                 num_entries += 1
                 if num_entries % 100000 == 0:
                     print("Written {} entries".format(num_entries))
+        print("Written dict for language code ", la_code)
 
 
 if __name__=="__main__":
