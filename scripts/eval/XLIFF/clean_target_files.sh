f54a1afd9049e4c8380755c57442fb5cf474b32f
Sophie Henning <s8sohenn@stud.uni-saarland.de>
2019-06-30 14:17:25 +0200

    Adapted XLIFF script to Irma's exceptions

--- scripts/eval/XLIFF/clean_target_files.sh
+++ scripts/eval/XLIFF/clean_target_files.sh
@@ -2,9 +2,17 @@
 
 sed 's/OR//g' query.tgt > query_man.tgt &&
 sed 's/AND//g' query_man.tgt > query_man2.tgt &&
+
+# filter Irma's Spanish translations of OR and AND...
+sed -E 's/\sO\s/ /g' query_man2.tgt > query_man.tgt &&
+sed -E 's/\sY\s/ /g' query_man.tgt > query_man2.tgt &&
+
+# filter publication year
 sed -E 's/PY(<|>)?=\"?[0-9]{4}\"?//g' query_man2.tgt > query_man.tgt &&
-sed -E 's/(TI|SW|ti|sw)\s?(=|:)\s?//g' query_man.tgt > query_man2.tgt &&
-sed -E 's/(AU|JT|DB|LA|DT|CM|SH)(=|:)"[^"]+"//g' query_man2.tgt > query_man.tgt &&
+
+# remove field names of translated fields
+sed -E 's/(TI|SW|ti|sw|SH)\s?(=|:)\s?//g' query_man.tgt > query_man2.tgt &&
+sed -E 's/(AU|JT|DB|LA|DT|CM)(=|:)\s?"[^"]+"//g' query_man2.tgt > query_man.tgt &&
 # do JT manually because there are journal titles that are longer than one token which are not enclosed by quotation marks
 sed -E 's/(AU|DB|LA|DT|CM|db)\s?(=|:)\s?\S+//g' query_man.tgt > query_man2.tgt &&
 
